35e12aa496ae5a0ad10b76c923b48b3ef4d85078
Ale-1407 <alessiacarrera07@gmail.com>
2023-01-17 16:26:47 +0100

    migration

---
+++ database/migrations/2023_01_17_141635_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('train_type', 100);
+            $table->string('station', 100);
+            $table->string('city', 100);
+            $table->time('departure');
+            $table->time('arrival');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
